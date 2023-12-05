ebadfe7ed010fc3f4e6df55506c526ab9ffb01eb
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2023-12-05 23:49:00 +0900

    공배수,마지막두원소

---
+++ algorithm/마지막두원소.swift
@@ -0,0 +1,8 @@
+//
+//  마지막두원소.swift
+//  algorithm
+//
+//  Created by 김정우 on 12/5/23.
+//
+
+import Foundation
---
+++ 공배수.swift
@@ -0,0 +1,8 @@
+//
+//  공배수.swift
+//  algorithm
+//
+//  Created by 김정우 on 12/5/23.
+//
+
+import Foundation
