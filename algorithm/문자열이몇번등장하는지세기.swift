78d453d6dc8e0004a3ba903cc830d280652f4f52
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2023-12-08 23:50:21 +0900

    문자열이몇번등장하는지세기

--- algorithm/문자열이몇번등장하는지세기.swift
+++ algorithm/문자열이몇번등장하는지세기.swift
@@ -1,8 +1,23 @@
-//
-//  문자열이몇번등장하는지세기.swift
-//  algorithm
-//
-//  Created by 김정우 on 12/8/23.
-//
-
 import Foundation
+
+func solution(_ myString:String, _ pat:String) -> Int {
+    var count = 0
+    let patLength = pat.count
+    let myStringLength = myString.count
+    
+    if patLength == 0 || patLength > myStringLength {
+        return 0
+    }
+    
+    for i in 0...(myStringLength - patLength) {
+        let startIndex = myString.index(myString.startIndex, offsetBy: i)
+        let endIndex = myString.index(startIndex, offsetBy: patLength)
+        let subString = myString[startIndex..<endIndex]
+        
+        if subString == pat {
+            count += 1
+        }
+    }
+    
+    return count
+}
