cc46302731f41fc1ec06c35a7de2d797b71620ef
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2023-12-09 23:49:46 +0900

    특정수열의특정한항만더하기

--- algorithm/등차수열의특정한항만더하기.swift
+++ algorithm/등차수열의특정한항만더하기.swift
@@ -1,8 +1,14 @@
-//
-//  등차수열의특정한항만더하기.swift
-//  algorithm
-//
-//  Created by 김정우 on 12/9/23.
-//
 
 import Foundation
+
+func solution(_ a:Int, _ d:Int, _ included:[Bool]) -> Int {
+    var answer = 0
+    var currentValue = a
+    for (index, isInclude) in included.enumerated() {
+        if isInclude {
+            answer += currentValue
+        }
+        currentValue += d
+    }
+    return answer
+}
