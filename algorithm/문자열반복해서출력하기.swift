c28dbe33c07e8f785e1cd90be7818c4d98766308
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2023-12-12 23:58:08 +0900

    문자열반복해서출력하기

--- algorithm/문자열반복해서출력하기.swift
+++ algorithm/문자열반복해서출력하기.swift
@@ -1,8 +1,17 @@
-//
-//  문자열반복해서출력하기.swift
-//  algorithm
-//
-//  Created by 김정우 on 12/12/23.
-//
-
 import Foundation
+
+func repeatString() {
+    let input = readLine()!.split(separator: " ")
+    let str = String(input[0])
+    let n = Int(input[1]) ?? 0
+    
+    if str.count >= 1 && str.count <= 10 && n >= 1 && n <= 5 {
+        let repeatedString = String(repeating: str, count: n)
+        print(repeatedString)
+    } else {
+        print("error")
+    }
+}
+func main() {
+    repeatString()
+}
