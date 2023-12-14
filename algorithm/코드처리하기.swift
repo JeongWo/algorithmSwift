cab48a0312c7ef36f65265ca68670cb8ac44ec48
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2023-12-14 23:05:04 +0900

    코드처리하기

---
+++ algorithm/코드처리하기.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+func solution(_ code:String) -> String {
+    var ret = ""
+    var mode = 0
+    
+    for (idx,char) in code.enumerated() {
+        if char == "1" {
+            mode = 1 - mode
+        } else {
+            if (mode == 0 && idx % 2 == 0) || (mode == 1 && idx % 2 == 1) {
+                ret.append(char)
+            }
+        }
+    }
+    if ret.isEmpty {
+        return "EMPTY"
+    } else {
+        return ret
+    }
+}
