f539375ce61406e1e8f5077d470439274b522642
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2023-12-20 23:28:02 +0900

    그림확대

---
+++ algorithm/그림확대.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ picture:[String], _ k:Int) -> [String] {
+    var answer:[String] = []
+    
+    for row in picture {
+        var newRow = ""
+        for pixel in row {
+            newRow += String(repeating: pixel, count: k)
+        }
+        for _ in 0..<k {
+            answer.append(newRow)
+        }
+    }
+    return answer
+}
