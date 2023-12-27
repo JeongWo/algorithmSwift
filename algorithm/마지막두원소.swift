97d475b5d8c4476628914b6dacc5d85b37cd77a9
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2023-12-27 23:54:53 +0900

    배열조각하기

--- algorithm/마지막두원소.swift
+++ algorithm/마지막두원소.swift
@@ -1,5 +1,5 @@
 import Foundation
-func solution(_ num_list:[Int]) -> [Int] {
+func solution1(_ num_list:[Int]) -> [Int] {
     var answer = num_list
     if num_list.last ?? 0 > (num_list.dropLast().last ?? 0) {
         answer.append(num_list.last! - (num_list.dropLast().last ?? 0))
--- algorithm/배열조각하기.swift
+++ algorithm/배열조각하기.swift
@@ -1,8 +1,12 @@
-//
-//  배열조각하기.swift
-//  algorithm
-//
-//  Created by 김정우 on 12/26/23.
-//
 
 import Foundation
+
+func _solution(_ arr:[Int], _ query:[Int]) -> [Int] {
+    return query.enumerated().reduce(into: arr) { arr, query in
+        if query.offset % 2 == 0 {
+            arr = arr.dropLast(arr.count - query.element - 1).map { $0 }
+        } else {
+            arr = arr.dropFirst(query.element).map { $0 }
+        }
+    }
+}
