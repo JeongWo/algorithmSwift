50fbe6e2eab034f1fa945ceb236d2057feaf52f4
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2023-12-31 15:09:27 +0900

    문자열 겹쳐쓰기

---
+++ algorithm/문자열겹쳐쓰기.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func _solution(_ my_string: String, _ overwrite_string: String, _ s: Int) -> String {
+    var answer = my_string
+    let start = my_string.index(my_string.startIndex, offsetBy: s)
+    let end = my_string.index(start, offsetBy: overwrite_string.count)
+    let range = start..<end
+
+    answer.replaceSubrange(range, with: overwrite_string)
+    return answer
+}
