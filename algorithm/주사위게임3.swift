3f70ab24b15f08d74efde0ce5c788bab81ea11b1
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-01-01 22:06:05 +0900

    주사위게임3

---
+++ algorithm/주사위게임3.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+func _solution(_ a:Int, _ b:Int, _ c:Int, _ d:Int) -> Int {
+    var countArray = Array(repeating: 0, count: 6)
+    countArray[a-1] += 1
+    countArray[b-1] += 1
+    countArray[c-1] += 1
+    countArray[d-1] += 1
+    
+    var answer = 0
+    
+    if countArray.contains(4) {
+        answer = 1111 * (countArray.firstIndex(of: 4)! + 1)
+    } else if countArray.contains(3) {
+        let p = Double(countArray.firstIndex(of: 3)! + 1)
+        let q = Double(countArray.firstIndex(of: 1)! + 1)
+        answer = Int(pow(10 * p + q, 2))
+    } else if countArray.filter({$0 == 2}).count == 2 {
+        let p = countArray.firstIndex(of: 2)! + 1
+        let q = countArray.lastIndex(of: 2)! + 1
+        answer = (p + q) * abs(p - q)
+    } else if countArray.filter({$0 == 2}).count == 1 && countArray.filter({$0 == 1}).count == 2 {
+        let q = countArray.firstIndex(of: 1)! + 1
+        let r = countArray.lastIndex(of: 1)! + 1
+        answer = q * r
+    } else {
+        answer = countArray.firstIndex(of: 1)! + 1
+    }
+    
+    return answer
+}
