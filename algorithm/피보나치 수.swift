8ce8d5d21afe8d4f8d37493e6d154a9c9620b316
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-01-12 12:45:06 +0900

    피보나치 수

---
+++ algorithm/피보나치 수.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func _solution(_ n: Int) -> Int {
+    var answer = [Int](repeating: 0, count: n+1)
+    
+    answer[0] = 0
+    answer[1] = 1
+    
+    for i in 2...n {
+        answer[i] = (answer[i-1] + answer[i-2] % 1234567)
+    }
+    return answer[n]
+}
