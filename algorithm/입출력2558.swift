a2ac6ea042e7195a025463bfe7fdda7d9f53bc5f
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-01-13 13:07:58 +0900

    input number2558

---
+++ algorithm/입출력2558.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func inputSolution2() {
+    let input1 = readLine()!
+    let input2 = readLine()!
+    
+    let A = Int(input1)!
+    let B = Int(input2)!
+    
+    print(A+B)
+    
+}
