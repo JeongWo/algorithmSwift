0d5d8650737a191e953fe18bed2ce899f38fc1cc
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-01-25 22:16:38 +0900

    좌표정렬하기

---
+++ algorithm/좌표정렬하기.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+
+func sortSoilution2() {
+    
+let N = Int(readLine()!)!
+
+var Arr: [(Int, Int)] = []
+
+for _ in 0..<N {
+    let a = readLine()!.split(separator: " ").map{Int($0)!}
+    Arr.append((a[0], a[1]))
+}
+
+Arr.sort(by: {
+    $0.0 == $1.0 ? $0.1 < $1.1 : $0.0 < $1.0
+})
+
+for x in 0..<N {
+    print("\(Arr[x].0) \(Arr[x].1)")
+    }
+}
