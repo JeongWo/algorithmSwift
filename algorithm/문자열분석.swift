1ab6b58a128942b5452d0f04713fee2d597fbd05
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-01-31 22:04:54 +0900

    문자열분석

--- algorithm/문자열분석.swift
+++ algorithm/문자열분석.swift
@@ -1,8 +1,24 @@
-//
-//  문자열분석.swift
-//  algorithm
-//
-//  Created by 김정우 on 1/31/24.
-//
-
 import Foundation
+
+func SolutionStr3() {
+    
+while let input = readLine() {
+            var lowercase = 0
+            var uppercase = 0
+            var digit = 0
+            var spc = 0
+            
+            for char in input {
+                if char.isLowercase {
+                    lowercase += 1
+                } else if char.isUppercase {
+                    uppercase += 1
+                } else if char.isNumber {
+                    digit += 1
+                } else if char.isWhitespace {
+                    spc += 1
+                }
+            }
+            print("\(lowercase) \(uppercase) \(digit) \(spc)")
+        }
+}
