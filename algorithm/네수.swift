e27e9f46dda5c8cc41a974d1cc70f6881b772a73
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-02-04 19:00:00 +0900

    네수

---
+++ algorithm/네수.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+
+func mathSolution2() {
+    
+if let input = readLine() {
+    let numbers = input.split(separator: " ").compactMap { Int($0) }
+    
+    guard numbers.count == 4 else {
+        fatalError("required number")
+    }
+    
+    let firstNum = Int("\(numbers[0])\(numbers[1])") ?? 0
+    let secondNum = Int("\(numbers[2])\(numbers[3])") ?? 0
+    
+    let answer = firstNum + secondNum
+    
+    print(answer)
+    }
+}
