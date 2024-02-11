2d2bd34c6a81efe567d7a5f1d7e818b8418bd138
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-02-11 21:40:46 +0900

    2진수8진수

---
+++ algorithm/2진수8진수.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func mathSolution9() {
+    
+var input = Array(readLine()!)
+
+if input.count%3 == 1 {
+    input.insert("0", at: 0)
+    input.insert("0", at: 0)
+} else if input.count%3 == 2 {
+    input.insert("0", at: 0)
+}
+
+for i in stride(from: 0, to: input.count-1, by: 3) {
+    let answer = Int(String(input[i]))!*4 + Int(String(input[i+1]))!*2 + Int(String(input[i+2]))!*1
+    print(answer, terminator: "")
+    }
+}
