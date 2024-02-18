e907e660d9a55004f7c878776c7901906d64aac7
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-02-18 22:17:21 +0900

    팩토리얼0의개수

---
+++ algorithm/팩토리얼0의개수.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+
+func mathSolution16() {
+    
+    func countT(_ N: Int) -> Int {
+        var count = 0
+        var divisor = 5
+        
+        while N / divisor > 0 {
+            count += N / divisor
+            divisor *= 5
+        }
+        
+        return count
+    }
+    
+    if let N = Int(readLine() ?? "0") {
+        print(countT(N))
+    }
+}
