9fe9f68590cfe5289487fbcc16bdf73d60255a0c
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-02-23 14:56:03 +0900

    stack

---
+++ algorithm/스택.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+func stackSolution() {
+    
+    enum CommandEnum: String {
+        case push
+        case pop
+        case size
+        case empty
+        case top
+    }
+    
+    let N = Int(readLine()!)!
+    var answer: [Int] = []
+    
+    for _ in 0..<N {
+        let input = readLine()!.split(separator: " ").map{String($0)}
+        guard let command = CommandEnum(rawValue: input[0]) else {break}
+        
+        switch command {
+        case .push:
+            answer.append(Int(input[1])!)
+        case .pop:
+            if answer.isEmpty {print(-1)} else {print(answer.removeLast())}
+        case .size:
+            print(answer.count)
+        case .empty:
+            if answer.isEmpty {print(1)} else {print(0)}
+        case .top:
+            if let topElement = answer.last {
+                print(topElement)
+            } else {print(-1)}
+        }
+        
+    }
+    
+    
+}
