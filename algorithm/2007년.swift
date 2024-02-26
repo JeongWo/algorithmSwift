476e7a179ca6dad370c98ad0ed5bc0bebad25878
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-02-26 17:12:09 +0900

    2007

---
+++ algorithm/2007년.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func inputSolution10() {
+    let input = readLine()!.split(separator: " ").map{Int($0)!}
+    let month = [0,0,31,28,31,30,31,30,31,31,30,31,30]
+    let day = ["MON","TUE","WED","THU","FRI","SAT","SUN"]
+    
+    print(day[(month[1...input[0]].reduce(0,+)+input[1]-1)%7])
+}
