e9beac4befaaa6a181fe46c9009d11c7ce4290b3
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-03-04 14:16:48 +0900

    별찍기5

---
+++ algorithm/별찍기5.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution17() {
+    
+    if let N = Int(readLine()!) {
+        for i in 1...N {
+            let spaces = String(repeating: " ", count: N - i)
+            let stars = String(repeating: "*", count: 2 * i - 1)
+            print(spaces + stars)
+        }
+    }
+}
