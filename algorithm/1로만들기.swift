66bf88e720916ea658d69509b3b4bdc9990477a3
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-03-10 15:53:04 +0900

    1로 만들기

---
+++ algorithm/1로만들기.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+func dpSolution1() {
+    
+    if let N = Int(readLine()!) {
+        if N > 1 {
+            
+            var dp = Array(repeating: 0, count: N+1)
+            
+            for i in 2...N {
+                dp[i] = dp[i - 1] + 1
+                if i % 2 == 0 {
+                    dp[i] = min(dp[i], dp[i/2] + 1)
+                }
+                if i % 3 == 0 {
+                    dp[i] = min(dp[i], dp[i/3] + 1)
+                }
+            }
+            print(dp[N])
+        } else {
+            print(0)
+        }
+    }
+}
