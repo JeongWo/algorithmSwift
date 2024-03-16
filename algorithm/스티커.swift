be55528d18cb597354ae7da9629bc67ce17e79f8
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-03-16 22:19:45 +0900

    스티커

--- algorithm/스티커.swift
+++ algorithm/스티커.swift
@@ -1,8 +1,32 @@
-//
-//  스티커.swift
-//  algorithm
-//
-//  Created by 김정우 on 3/16/24.
-//
-
 import Foundation
+
+func dpSolution7() {
+    
+    func maxStickerScore(_ stickers: [[Int]]) -> Int {
+        let n = stickers[0].count
+        var dp = Array(repeating: Array(repeating: 0, count: n), count: 3)
+        
+        dp[0][0] = stickers[0][0]
+        dp[1][0] = stickers[1][0]
+        
+        for i in 1..<n {
+            dp[0][i] = max(dp[1][i-1], dp[2][i-1]) + stickers[0][i]
+            dp[1][i] = max(dp[0][i-1], dp[2][i-1]) + stickers[1][i]
+            dp[2][i] = max(dp[0][i-1], dp[1][i-1])
+        }
+        return max(dp[0][n-1], dp[1][n-1], dp[2][n-1])
+    }
+    
+    let T = Int(readLine()!)!
+    
+    for _ in 0..<T {
+        let n = Int(readLine()!)!
+        var stickers = [[Int]]()
+        for _ in 0..<2 {
+            let row = readLine()!.split(separator: " ").map { Int($0)! }
+            stickers.append(row)
+        }
+        let result = maxStickerScore(stickers)
+        print(result)
+    }
+}
