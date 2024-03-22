ba9a768443101cb4d2ebf50598bcea40e91b4ce9
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-03-22 15:41:00 +0900

    계단오르기

--- algorithm/계단오르기.swift
+++ algorithm/계단오르기.swift
@@ -1,8 +1,31 @@
-//
-//  계단오르기.swift
-//  algorithm
-//
-//  Created by 김정우 on 3/22/24.
-//
-
 import Foundation
+
+func dpSolution12() {
+    
+    let n = Int(readLine()!)!
+    var arr = [0]
+    
+    for _ in 0..<n {
+        arr.append(Int(readLine()!)!)
+    }
+    
+    var dp = Array(repeating: 0, count: 301)
+    
+    if n == 1 {
+        print(arr[1])
+    } else if n == 2 {
+        print(arr[1] + arr[2])
+    } else if n == 3 {
+        print(max(arr[1]+arr[3], arr[2]+arr[3]))
+    } else {
+        dp[1] = arr[1]
+        dp[2] = arr[1] + arr[2]
+        dp[3] = max(arr[1]+arr[3], arr[2]+arr[3])
+        
+        for i in 4...n {
+            dp[i] = max(arr[i]+arr[i-1]+dp[i-3], dp[i-2]+arr[i])
+        }
+        
+        print(dp[n])
+    }
+}
