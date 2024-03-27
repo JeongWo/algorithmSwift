7d0384dd1c299aac996b1ef1f6f4ab5a1c99405d
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-03-27 12:32:12 +0900

    카드구매하기

---
+++ algorithm/카드구매하기.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func buyingCard() {
+    
+    if let N = Int(readLine()!) {
+        let prices = readLine()!.split(separator: " ").map{Int($0)!}
+        var dp = Array(repeating: 0, count: N+1)
+        
+        for i in 1...N {
+            for j in 1...i {
+                dp[i] = max(dp[i], dp[i-j] + prices[j-1])
+            }
+        }
+        print(dp[N])
+        
+    }
+}
