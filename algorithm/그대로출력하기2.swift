de0b82ee639858f297d7c619161ae36f26cca77c
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-03-31 23:01:11 +0900

    그대로출력하기2

---
+++ algorithm/그대로출력하기2.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func inputSolution17() {
+    
+    var inputLines: [String] = []
+    
+    while let line = readLine() {
+        inputLines.append(line)
+    }
+    
+    for line in inputLines {
+        print(line)
+    }
+}
