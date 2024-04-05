aa8127bbe0ea1c0c6b3e3951295347f0e7c98f4a
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-05 16:41:49 +0900

    30

---
+++ algorithm/30.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+func thirty() {
+    
+    if let N = readLine() {
+        let digits = Array(N).compactMap{Int(String($0))}
+        
+        var sum = 0
+        for digit in digits {
+            sum += digit
+        }
+        
+        guard sum % 3 == 0 else {
+            print("-1")
+            exit (0)
+        }
+        
+        var sortedDigits = digits.sorted(by: >)
+        
+        guard sortedDigits.contains(0) else {
+            print("-1")
+            exit (0)
+        }
+        
+        let result = sortedDigits.map{String($0)}.joined()
+        print(result)
+    }
+}
