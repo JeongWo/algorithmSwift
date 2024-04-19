9b256f6f6fa5093b733627cb9c8c4659778cf716
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-20 01:01:04 +0900

    대회or인턴r

---
+++ algorithm/대회or인턴r.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func Contest() {
+    
+    func teamCount(N: Int, M: Int, K:Int) -> Int {
+        let team = min(N / 2 , M)
+        let maxTeams = min(team, (N + M - K) / 3)
+        
+        return maxTeams
+    }
+    
+    if let input = readLine()?.split(separator: " ").compactMap({Int($0)}), input.count == 3 {
+        let N = input[0]
+        let M = input[1]
+        let K = input[2]
+        let result = teamCount(N: N, M: M, K: K)
+        print(result)
+    }
+    
+}
