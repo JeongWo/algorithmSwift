96e26c670d1c6dc0356fc4107c83b75bad5c26cf
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-05-06 23:50:02 +0900

    배열조각하기r

---
+++ algorithm/배열조각하기r.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func arrayR(_ arr:[Int], _ query:[Int]) -> [Int] {
+    return query.enumerated().reduce(into: arr) { arr, query in
+        if query.offset % 2 == 0 {
+            arr = arr.dropLast(arr.count - query.element - 1).map { $0 }
+        } else {
+            arr = arr.dropFirst(query.element).map { $0 }
+        }
+    }
+}
