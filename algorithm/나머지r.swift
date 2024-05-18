2f1236ece1b7e7795e1cdeacb41aa9ae93fc7ef2
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-05-18 16:17:38 +0900

    나머지r

---
+++ algorithm/나머지r.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+func rest() {
+    
+    if let input = readLine() {
+        let numbers = input.split(separator: " ").compactMap { Int($0) }
+        
+        if numbers.count == 3 {
+            let A = numbers[0]
+            let B = numbers[1]
+            let C = numbers[2]
+            
+            let result1 = (A + B) % C
+            let result2 = ((A % C) + (B % C)) % C
+            let result3 = (A * B) % C
+            let result4 = ((A % C) * (B % C)) % C
+            
+            print(result1)
+            print(result2)
+            print(result3)
+            print(result4)
+        }
+    }
+}
