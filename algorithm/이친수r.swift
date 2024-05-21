9f0e08043a41648ba6f2f2cd402a310512fc8e1e
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-05-21 22:01:29 +0900

    이친수r

---
+++ algorithm/이친수r.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+func dpNum() {
+    
+    if let N = Int(readLine()!) {
+        var dp = [[Int]](repeating: Array(repeating: 0, count: 2), count: N+1)
+        
+        if N == 1 {
+            print(1)
+        } else {
+            dp[1][0] = 0
+            dp[1][1] = 1
+            
+            for i in 2...N {
+                dp[i][0] = dp[i-1][0] + dp[i-1][1]
+                dp[i][1] = dp[i-1][0]
+            }
+            print(dp[N][0] + dp[N][1])
+        }
+    }
+}
