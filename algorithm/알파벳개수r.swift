56ec1fdc675a436c3fa23c5aeff751aebbb789fe
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-06-09 23:39:55 +0900

    알파벳개수r

---
+++ algorithm/알파벳개수r.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+
+func numberOfAlphabet() {
+    
+    let alphabet = Array("abcdefghijklmnopqrstuvwxyz")
+    var answer = Array(repeating: "0", count: 26)
+    let input = readLine()!
+    var S = [String]()
+    
+    for i in input {
+        if !S.contains(String(i)) {
+            var temp = 0
+            for j in input {
+                if i == j {temp += 1}
+            }
+            answer[alphabet.firstIndex(of: i)!] = String(temp)
+        }
+        S.append(String(i))
+    }
+    print(answer.joined(separator: " "))
+}
