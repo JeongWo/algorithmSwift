8d0a6ba31e9cf2a1df497cc813da87bb30f15365
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-06-12 23:57:05 +0900

    최대공약수r

---
+++ algorithm/최대공약수r.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+func MaximumCommitment() {
+    
+    func gcd(_ a: Int, _ b: Int) -> Int {
+        if b == 0 {
+            return a
+        } else {
+            return gcd(b, a % b)
+        }
+    }
+    
+    
+    if let input = readLine()?.split(separator: " ").compactMap({ Int($0) }) {
+        let A = input[0]
+        let B = input[1]
+        
+        let gcdV = gcd(A, B)
+        let answer = String(repeating: "1", count: gcdV)
+        
+        print(answer)
+    }
+    
+}
