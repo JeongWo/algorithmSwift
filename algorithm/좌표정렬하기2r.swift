7c6617ff9b66d43c95f9a9a031cad9a31b6bae0d
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-06-18 18:36:48 +0900

    좌표정렬하기2r

---
+++ algorithm/좌표정렬하기2r.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+func AligningCoordinates() {
+    
+    let N = Int(readLine()!)!
+    
+    var Arr: [(Int, Int)] = []
+    
+    for _ in 0..<N {
+        let a = readLine()!.split(separator: " ").map{Int($0)!}
+        Arr.append((a[0],a[1]))
+    }
+    
+    Arr.sort(by: {
+        $0.1 == $1.1 ? $0.0 < $1.0 : $0.1 < $1.1
+    })
+    
+    for y in 0..<N {
+        print("\(Arr[y].0) \(Arr[y].1)")
+    }
+    
+}
