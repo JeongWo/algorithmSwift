088f1ddd7c083c49f9de15d2c04460a400312552
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-07-25 22:52:13 +0900

    fbi

--- algorithm/FBI.swift
+++ algorithm/FBI.swift
@@ -1,8 +1,21 @@
-//
-//  FBI.swift
-//  algorithm
-//
-//  Created by 김정우 on 7/25/24.
-//
-
 import Foundation
+
+
+func fbi() {
+    var results = [Int]()
+    
+    for i in 1...5 {
+        if let agentName = readLine() {
+            if agentName.contains("FBI") {
+                results.append(i)
+            }
+        }
+    }
+    
+    if results.isEmpty {
+        print("HE GOT AWAY!")
+    } else {
+        let output = results.map { String($0) }.joined(separator: " ")
+        print(output)
+    }
+}
